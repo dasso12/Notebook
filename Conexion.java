cc1e40ba52743402cd6637827223f75fac7c0242
dasso12 <48688605+dasso12@users.noreply.github.com>
2019-04-02 16:33:10 -0600

    conexion
    bd conexion

---
+++ Conexion.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo;
+
+import java.sql.*;
+
+/**
+ *
+ * @author A D A N
+ */
+public class Conexion {
+    
+   static String bd = "test";
+   static String login = "root";
+   static String password = "stereo";
+   static String url = "jdbc:mysql://localhost/" + bd;
+
+   Connection conn = null;
+
+   /** Constructor de DbConnection */
+   public Conexion() {
+      try{
+         //obtenemos el driver de para mysql
+         Class.forName("com.mysql.jdbc.Driver");
+         //obtenemos la conexi�n
+         conn = DriverManager.getConnection(url,login,password);
+
+         if (conn!=null){
+            System.out.println("Conección a base de datos "+bd+" OK");
+         }
+      }
+      catch(SQLException e){
+         System.out.println(e);
+      }catch(ClassNotFoundException e){
+         System.out.println(e);
+      }catch(Exception e){
+         System.out.println(e);
+      }
+   }
+   /**Permite retornar la conexi�n*/
+   public Connection getConnection(){
+      return conn;
+   }
+
+   public void desconectar(){
+      conn = null;
+   }
+    
+}
