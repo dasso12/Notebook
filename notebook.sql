39a61da65e33a8a0f4947d48b493915f119c7343
dasso12 <48688605+dasso12@users.noreply.github.com>
2019-04-04 14:30:35 -0600

    Database Query Notebook

--- notebook.sql
+++ notebook.sql
@@ -1,7 +1,5 @@
 CREATE DATABASE notebook;
 
--- DROP DATABASE notebook;
-
 USE notebook;
 
 CREATE TABLE login(
@@ -10,34 +8,16 @@ user_name varchar (30)not null,
 pass_name varchar (15)not null
 );
 
-CREATE TABLE contact(
-id_contact int primary key auto_increment not null,
-description varchar (30)
-);
-
-CREATE TABLE  sport(
-id_sport int primary key auto_increment not null,
-description varchar (20)
-);
-
-CREATE TABLE  specialty(
-id_specialty int primary key auto_increment not null,
-description varchar (20)
-);
-
 CREATE TABLE athlete(
 id_athlete int primary key auto_increment not null,
 athlete_name varchar (20) not null,
 father_surname varchar (25) not null,
 mother_surname varchar (25),
-gender boolean not null,
-birthdate date,
-
-id_sport int not null,
-foreign key (id_sport) references sport (id_sport),
-
-id_contact int not null,
-foreign key (id_contact) references contact (id_contact)
+gender varchar (20),
+birthdate varchar (15),
+cellphone_number varchar (45),
+telephone_home varchar (45),
+email varchar (45) not null
 );
 
 CREATE TABLE doctor(
@@ -45,16 +25,12 @@ id_doctor int primary key auto_increment not null,
 doctor_name varchar (20) not null,
 father_surname varchar (25) not null,
 mother_surname varchar (25),
-birthdate date not null,
-
-id_contact int not null,
-foreign key (id_contact) references contact (id_contact),
-
-id_specialty int not null,
-foreign key (id_specialty) references specialty (id_specialty),
-
+birthdate varchar (15) not null,
 hospital_name varchar (55),
-hospital_adress varchar (90)
+hospital_adress varchar (90),
+cellphone_number varchar (45),
+telephone_home varchar (45),
+email varchar (45) not null
 );
 
 CREATE TABLE student(
@@ -64,12 +40,11 @@ father_surname varchar (25) not null,
 mother_surname varchar (25),
 country_of_birth varchar (60) not null,
 city_of_birth varchar (60) not null,
-birthdate date,
+birthdate varchar (15),
 career varchar (50) not null,
 email varchar (50) not null,
-
-id_contact int not null,
-foreign key (id_contact) references contact (id_contact)
+cellphone_number varchar (45),
+telephone_home varchar (45)
 );
 
 CREATE TABLE graduate (
@@ -78,10 +53,10 @@ graduate_name varchar (20) not null,
 father_surname varchar (25) not null,
 mother_surname varchar (25),
 office_telephone varchar (25),
-schedule_of_attention datetime not null,
-
-id_contact int not null,
-foreign key (id_contact) references contact (id_contact)
+schedule_of_attention varchar (45) not null,
+cellphone_number varchar (45),
+telephone_home varchar (45),
+email varchar (45) not null
 );
 
 CREATE TABLE person(
@@ -89,12 +64,14 @@ id_person int primary key auto_increment not null,
 person_name varchar (20),
 father_surname varchar (25) not null,
 mother_surname varchar (25),
-
-id_contact int not null,
-foreign key (id_contact) references contact (id_contact),
-
 adress varchar (90) not null,
 favorite_contact varchar (20),
-occupation varchar (30) not null
+occupation varchar (30) not null,
+cellphone_number varchar (45),
+telephone_home varchar (45),
+email varchar (45) not null
 );
 
+
+DROP DATABASE notebook;
+SELECT * FROM person;
